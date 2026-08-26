a4b60ba977c60665c8ebd6079a405b6353249e25
jcchang13-a11y <jcchang13@gmail.com>
2026-08-26 20:59:57 +0800

    Bump Guixu Android app to v0.3

--- android/guixu/app/build.gradle.kts
+++ android/guixu/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "tw.guixu.underground"
         minSdk = 23
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
     }
 }
 
