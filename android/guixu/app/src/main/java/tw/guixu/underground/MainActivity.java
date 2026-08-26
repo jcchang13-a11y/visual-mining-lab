8d36280fe35fb8b9f9270576a6e7346761c93fd9
jcchang13-a11y <jcchang13@gmail.com>
2026-08-26 19:11:35 +0800

    Replace WebView wrapper with Chrome Custom Tab

--- android/guixu/app/src/main/java/tw/guixu/underground/MainActivity.java
+++ android/guixu/app/src/main/java/tw/guixu/underground/MainActivity.java
@@ -1,66 +1,28 @@
 package tw.guixu.underground;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
-import android.webkit.WebChromeClient;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import androidx.browser.customtabs.CustomTabColorSchemeParams;
+import androidx.browser.customtabs.CustomTabsIntent;
 
 public class MainActivity extends Activity {
-    private WebView webView;
     private static final String START_URL = "https://jcchang13-a11y.github.io/visual-mining-lab/immortalia/underground/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        webView = new WebView(this);
-        setContentView(webView);
+        CustomTabColorSchemeParams params = new CustomTabColorSchemeParams.Builder()
+                .setToolbarColor(0xFF171817)
+                .setNavigationBarColor(0xFF171817)
+                .build();
 
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
+        CustomTabsIntent customTabsIntent = new CustomTabsIntent.Builder()
+                .setDefaultColorSchemeParams(params)
+                .setShowTitle(false)
+                .build();
 
-        // The website already defines width=device-width. Do not let WebView
-        // apply a second overview-fit zoom pass, which distorts responsive UI.
-        settings.setLoadWithOverviewMode(false);
-        settings.setUseWideViewPort(true);
-        settings.setSupportZoom(false);
-        settings.setBuiltInZoomControls(false);
-        settings.setDisplayZoomControls(false);
-        settings.setTextZoom(100);
-
-        webView.setInitialScale(0);
-        webView.setWebChromeClient(new WebChromeClient());
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                view.loadUrl(request.getUrl().toString());
-                return true;
-            }
-        });
-
-        if (savedInstanceState == null) {
-            webView.loadUrl(START_URL);
-        } else {
-            webView.restoreState(savedInstanceState);
-        }
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        webView.saveState(outState);
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (webView != null && webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
+        customTabsIntent.launchUrl(this, Uri.parse(START_URL));
     }
 }
